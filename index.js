1b578f7b166532aee16441b24435170eec6f8ea3
Timothy Ayodele <timotech@yahoo.com>
2020-04-05 10:49:57 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = new express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+const botName = "Admin";
+
+app.get("/", function (req, res) {
+  res.redirect("index.html");
+});
+
+io.on("connection", function (socket) {
+  socket.on("stream", function (image) {
+    socket.broadcast.emit("stream", image);
+  });
+
+  //Join Room
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to Class!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the class`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the class`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+http.listen(port, () => console.log(`Server running on port ${port}`));
